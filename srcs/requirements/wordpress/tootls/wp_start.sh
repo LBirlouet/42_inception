2dd9119510af160026a39d78517c2b8f6c86a318
Lucas Birlouet <lucasbirlouer@MacBook-Air-de-Lucas.local>
2025-06-18 15:25:55 +0200

    modif: modif doss wordpress, need to check at 19 if all is working

---
+++ srcs/requirements/wordpress/tootls/wp_start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+sleep 10
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+cd /var/www/html
+
+if [ ! -s "wp-config.php" ]; then
+
+    echo "Preparing WordPress installation..."
+    wp core download --allow-root
+
+    echo "Generating wp-config.php file..."
+    wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD --dbhost=$WORDPRESS_DB_HOST --skip-check
+
+    echo "Installing WordPress..."
+    wp core install --allow-root --url=$DOMAIN_NAME --title=$WORDPRESS_DB_NAME --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_MAIL
+
+    echo "Adding a new WordPress user..."
+    wp user create "$WORDPRESS_USER" "$WORDPRESS_USER_MAIL" --role=subscriber --user_pass="$WORDPRESS_USER_PASSWORD" --allow-root --path=/var/www/html
+
+else
+    echo "WordPress is already set up and ready to use."
+fi
+
+exec /usr/sbin/php-fpm7.4 -F
