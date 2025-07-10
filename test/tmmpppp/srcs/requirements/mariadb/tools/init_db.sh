6890915ed815772ba675a57a6c8228f351e3b767
Lucas Birlouet <lbirloue@shi-r6-p6.s19.be>
2025-07-10 11:53:20 +0200

    modif

---
+++ test/tmmpppp/srcs/requirements/mariadb/tools/init_db.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir -p /var/run/mysqld
+chown -R mysql:mysql /var/run/mysqld
+mysqld_safe &
+while ! mysqladmin ping --silent; do
+    sleep 1
+done
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" <<-EOF
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
+exec mysqld_safe
---
+++ test/tmmpppp/srcs/requirements/wordpress/tools/start_wp.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "En attente de la base de données..."
+sleep 10
+echo "Base de données accessible."
+
+chown -R www-data:www-data /var/www/html
+chmod -R 755 /var/www/html
+
+cd /var/www/html
+
+if [ ! -s "wp-config.php" ]
+then
+    echo "Téléchargement de Wordpress..."
+	wp core download --allow-root
+
+	echo "Création du fichier wp-config.php..."
+	wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --skip-check
+
+	echo "Installation de WordPress..."
+	wp core install --allow-root --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL
+
+	echo "Création d'un nouvel utilisateur"
+	wp user create "$WP_USER" "$WP_USER_EMAIL" --role=subscriber --user_pass="$WP_USER_PASSWORD" --allow-root --path=/var/www/html
+else
+    echo "WordPress déjà installé."
+fi
+
+echo "Démarrage de PHP-FPM..."
+exec /usr/sbin/php-fpm7.4 -F
